a7636bf9df57a953df719bfde2e168253578771e
Giovanni Alberto Salas <gioatondo7@gmail.com>
2023-05-31 16:51:18 -0700

    Making the Request

--- src/app/fetchphotos.service.ts
+++ src/app/fetchphotos.service.ts
@@ -6,5 +6,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchphotosService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+
+
+  }
+
+  getPhoto(){
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID DWRqYfZggUNTm9Zw06Oxlx4xYbfuSwc40SBxe3tce8g'
+      }
+    });
+
+  }
 }
